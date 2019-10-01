c4a4488dc47bf874fa53ff3d72d81e9c95916b1f
thiernodiallo222 <thiernodiallo864@gmail.com>
2019-09-30 23:05:02 -0400

    just commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// Defining a function that prints a given string
+
+function print(a){
+	console.log(m);
+}
+
+
+//define a string variable
+let m = "This is the first funtion";
+
+
+//calling the function
+print(m);
